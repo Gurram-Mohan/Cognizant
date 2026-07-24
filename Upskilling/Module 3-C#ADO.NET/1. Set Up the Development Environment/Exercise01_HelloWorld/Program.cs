00c6a35c1b32382d6081f5a569b64a7c13c65f80
Gurram Mohan <mohangurram700@gmail.com>
2026-07-24 23:50:47 +0530

    Add files via upload

---
+++ Upskilling/Module 3-C#ADO.NET/1. Set Up the Development Environment/Exercise01_HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercise01_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
