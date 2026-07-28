3f12731581272431b70569cad1978441d1b90cb8
Gurram Mohan <mohangurram700@gmail.com>
2026-07-28 22:21:54 +0530

    Add files via upload

---
+++ Upskilling/Module 3-C#ADO.NET/3. Use Primary Constructors in C# 12/Exercise03_PrimaryConstructors/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercise03_PrimaryConstructors
+{
+    class Person(string name, int age)
+    {
+        // Auto-implemented properties
+        public string Name { get; set; } = name;
+        public int Age { get; set; } = age;
+
+
+        // Method to display full information
+        public void DisplayInformation()
+        {
+            Console.WriteLine("Person Details");
+            Console.WriteLine("----------------");
+            Console.WriteLine($"Name : {Name}");
+            Console.WriteLine($"Age  : {Age}");
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating object using primary constructor
+            Person person = new Person("Mohan", 21);
+
+            // Display details
+            person.DisplayInformation();
+        }
+    }
+}
