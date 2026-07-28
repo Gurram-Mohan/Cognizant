29477cc9d6508bc1259909df08d851c5c079b679
Gurram Mohan <mohangurram700@gmail.com>
2026-07-28 21:54:45 +0530

    Add files via upload

---
+++ Upskilling/Module 3-C#ADO.NET/2. Explore Value vs Reference Types/Exercise02_ValueVsReference/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Exercise02_ValueVsReference
+{
+    class Person
+    {
+        public string Name { get; set; }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Value Type Example
+            int number = 10;
+
+            Console.WriteLine("Before modifying value type:");
+            Console.WriteLine($"Number = {number}");
+
+            ModifyValueType(number);
+
+            Console.WriteLine("After modifying value type:");
+            Console.WriteLine($"Number = {number}");
+
+
+            Console.WriteLine("------------------------------");
+
+
+            // Reference Type Example
+            Person person = new Person();
+            person.Name = "Mohan";
+
+            Console.WriteLine("Before modifying reference type:");
+            Console.WriteLine($"Name = {person.Name}");
+
+            ModifyReferenceType(person);
+
+            Console.WriteLine("After modifying reference type:");
+            Console.WriteLine($"Name = {person.Name}");
+        }
+
+
+        // Method for Value Type
+        static void ModifyValueType(int value)
+        {
+            value = 100;
+        }
+
+
+        // Method for Reference Type
+        static void ModifyReferenceType(Person person)
+        {
+            person.Name = "Gurram Mohan";
+        }
+    }
+}
