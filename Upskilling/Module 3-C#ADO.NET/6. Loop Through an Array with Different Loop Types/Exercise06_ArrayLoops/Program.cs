efa9b7e1155da909b3723c9ca8d636b5d8de1fa5
Gurram Mohan <mohangurram700@gmail.com>
2026-08-05 12:08:17 +0530

    Add files via upload

---
+++ Upskilling/Module 3-C#ADO.NET/6. Loop Through an Array with Different Loop Types/Exercise06_ArrayLoops/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Exercise06_ArrayLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 20, 30, 40, 50, 60 };
+
+            Console.WriteLine("=== FOR LOOP ===");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == 30)
+                {
+                    Console.WriteLine("Skipping 30");
+                    continue;
+                }
+
+                Console.WriteLine(numbers[i]);
+
+                if (numbers[i] == 50)
+                {
+                    Console.WriteLine("Stopping at 50");
+                    break;
+                }
+            }
+
+            Console.WriteLine("\n=== FOREACH LOOP ===");
+
+            foreach (int num in numbers)
+            {
+                if (num == 30)
+                {
+                    Console.WriteLine("Skipping 30");
+                    continue;
+                }
+
+                Console.WriteLine(num);
+
+                if (num == 50)
+                {
+                    Console.WriteLine("Stopping at 50");
+                    break;
+                }
+            }
+
+            Console.WriteLine("\n=== WHILE LOOP ===");
+
+            int index = 0;
+
+            while (index < numbers.Length)
+            {
+                if (numbers[index] == 30)
+                {
+                    Console.WriteLine("Skipping 30");
+                    index++;
+                    continue;
+                }
+
+                Console.WriteLine(numbers[index]);
+
+                if (numbers[index] == 50)
+                {
+                    Console.WriteLine("Stopping at 50");
+                    break;
+                }
+
+                index++;
+            }
+
+            Console.WriteLine("\n=== DO-WHILE LOOP ===");
+
+            index = 0;
+
+            do
+            {
+                if (numbers[index] == 30)
+                {
+                    Console.WriteLine("Skipping 30");
+                    index++;
+                    continue;
+                }
+
+                Console.WriteLine(numbers[index]);
+
+                if (numbers[index] == 50)
+                {
+                    Console.WriteLine("Stopping at 50");
+                    break;
+                }
+
+                index++;
+
+            } while (index < numbers.Length);
+
+            Console.ReadKey();
+        }
+    }
+}
