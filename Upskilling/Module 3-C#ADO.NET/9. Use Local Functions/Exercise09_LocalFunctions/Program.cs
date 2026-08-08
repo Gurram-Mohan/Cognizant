10effc985d6fb8e59be8ae7b62215218803a7cba
Gurram Mohan <mohangurram700@gmail.com>
2026-08-08 15:32:30 +0530

    Add files via upload

---
+++ Upskilling/Module 3-C#ADO.NET/9. Use Local Functions/Exercise09_LocalFunctions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercise09_LocalFunctions
+{
+    internal class Program
+    {
+        static int CalculateFactorial(int number)
+        {
+            // Local function
+            int Factorial(int n)
+            {
+                if (n <= 1)
+                {
+                    return 1;
+                }
+
+                return n * Factorial(n - 1);
+            }
+
+            // Calling the local function
+            return Factorial(number);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int result = CalculateFactorial(number);
+
+            Console.WriteLine($"Factorial of {number} = {result}");
+
+            Console.ReadKey();
+        }
+    }
+}
