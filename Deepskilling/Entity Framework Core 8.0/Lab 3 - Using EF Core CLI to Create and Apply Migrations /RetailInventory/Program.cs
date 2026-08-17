184886d6d4fc2db26ea39f281def1dfd518cb6a9
Gurram Mohan <mohangurram700@gmail.com>
2026-08-17 23:00:56 +0530

    Delete Deepskilling/Entity Framework Core 8.0/Lab 3 - Using EF Core CLI to Create and Apply Migrations  directory

--- Deepskilling/Entity Framework Core 8.0/Lab 3 - Using EF Core CLI to Create and Apply Migrations /RetailInventory/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using RetailInventory.Data;
-
-Console.WriteLine("Retail Inventory System");
-Console.WriteLine("Database Context Configured Successfully!");
-
-AppDbContext context = new AppDbContext();
-Console.ReadLine();
